749cf134cbc313ae43763c93d47c8c279416193c
Komșa Attila <63514374+kms77@users.noreply.github.com>
2022-03-12 13:44:18 +0200

    Added Flask Update for Heroku

--- app.py
+++ app.py
@@ -1,24 +1,4 @@
-from flask import Flask
-import os
-from flask import send_from_directory
-
-app = Flask(__name__)
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/favicon.png')
-
-@app.route('/home')
-def home():
-    return "Hello World Home"
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World ROOT!'
-
-
-
+from app.main import app
 
 if __name__ == '__main__':
     app.run()
---
+++ app/main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import os
+from flask import send_from_directory
+
+app = Flask(__name__)
+
+@app.route("/")
+def root_page():
+    return "<h1>Root Page</h1>"
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/favicon.png')
