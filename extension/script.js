4420acc39212aef3b40dc25478ed3c096cbdc741
Komșa Attila <63514374+kms77@users.noreply.github.com>
2022-03-20 20:57:12 +0200

    Update

--- extension/script.js
+++ extension/script.js
@@ -1,3 +1,26 @@
 import axios from '../node_modules/axios/dist/axios'
 
-//document.getElementById("request-button-id").addEventListener("click", sendRequest);
+document.getElementById("request-button-id").addEventListener("click", sendRequest);
+// function sendRequest(){
+         //let textToSend = document.getElementById("header-text-id");
+         //console.log("Text to send: ", textToSend);
+//         axios({
+//                 method: "post",
+//                 url: "https://test-python-heroku-app.herokuapp.com/data/",
+//                 data: {
+//                   message: "test"
+//                 }
+//         });
+// }
+
+function sendRequest(){
+        $.ajax({
+                url: "https://test-python-heroku-app.herokuapp/data/",
+                data: {
+                   message: "test"
+                 },
+                type: "POST",
+                dataType: "json"
+
+        });
+}
