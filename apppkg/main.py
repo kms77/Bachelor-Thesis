d0e57a5101cf4178cd7b52d3590db789db50c764
Komșa Attila <63514374+kms77@users.noreply.github.com>
2022-03-20 19:14:45 +0200

    Update main.py

--- apppkg/main.py
+++ apppkg/main.py
@@ -1,10 +1,15 @@
 from __future__ import print_function
-import sys
 from flask import Flask, request
 import os
 from flask import send_from_directory
 
 app = Flask(__name__)
+
+@app.route("/data", methods=["GET", "POST"])
+def get_data():
+    request_data = request.json
+    message = request_data['message']
+    return "<h3>The message is: {}</h3>".format(message)     
 
 @app.route("/")
 def root_page():
@@ -14,10 +19,4 @@ def root_page():
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/favicon.png')
-
-@app.route("/data", methods=["GET", "POST"])
-def get_data():
-    request_data = request.json
-    print('Hello world! + {}'.format(request_data), file=sys.stderr)
-    message = request_data['message']
-    return "<h3>The message is: {}</h3>".format(message)                              
+                         
