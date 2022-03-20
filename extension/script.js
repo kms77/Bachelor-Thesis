aacc0106d70696ffb5d65b09f0835cc3f8d5bdfd
Komșa Attila <63514374+kms77@users.noreply.github.com>
2022-03-20 20:04:21 +0200

    Added script

--- extension/script.js
+++ extension/script.js
@@ -1,14 +1,3 @@
 import axios from '../node_modules/axios/dist/axios'
 
-document.getElementById("request-button-id").addEventListener("click", sendRequest);
-function sendRequest(){
-        //let textToSend = document.getElementById("header-text-id");
-        //console.log("Text to send: ", textToSend);
-        axios({
-                method: "post",
-                url: "https://test-python-heroku-app.herokuapp.com/data/",
-                data: {
-                  message: "teest"
-                }
-        });
-}
+//document.getElementById("request-button-id").addEventListener("click", sendRequest);
