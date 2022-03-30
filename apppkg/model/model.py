43aa4f33c9f7aa87618be3bb52d4f318b0ff7675
Komșa Attila <komsaatti@yahoo.com>
2022-03-30 17:24:37 +0300

    Added generate_beam method

--- apppkg/model/model.py
+++ apppkg/model/model.py
@@ -1,8 +1,37 @@
-import torch
+import clip
+import os
 from torch import nn
-from typing import Tuple, Optional
+import numpy as np
+import torch
+import torch.nn.functional as nnf
+import sys
+from typing import Tuple, List, Union, Optional
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, AdamW, get_linear_schedule_with_warmup
+from tqdm import tqdm, trange
+import skimage.io as io
+import PIL.Image
+from IPython.display import Image
 
-from transformers import GPT2LMHeadModel
+N = type(None)  # N = NoneType(type of the None object - it indicates no value)
+A = np.array  # array
+# np.ndarray = associated data-type object describes the format of each element in the array
+SA = np.ndarray  # single array
+MSA = Union[Tuple[SA, ...], List[SA]]  # multiple single arrays
+MA = Union[Tuple[A, ...], List[A]]  # multiple arrays
+AN = Union[A, N]  # array U NoneType
+MAN = Union[MA, N]  # multiple single arrays U NoneType
+T = torch.Tensor  # a multi-dimensional matrix containing elements of a single data type
+TM = Union[Tuple[T, ...], List[T]]  # multiple multi-dimensional matrices
+TN = Optional[T]  # multi-dimensional matrix or NoneType
+TNM = Union[Tuple[TN, ...], List[TN]]  # multi-dimensional matrices or NoneType
+TMN = Optional[TM]  # multiple multi-dimensional matrices or NoneType
+TSA = Union[T, SA]  # multi-dimensional matrix U single array
+
+current_directory = os.getcwd()  # returns the absolute path of the working directory where Python is currently running
+save_path = os.path.join(os.path.dirname(current_directory), "pretrained_models")
+os.makedirs(save_path, exist_ok=True)
+# os.path.join = returns a string which represents the concatenated path components
+model_path = os.path.join(save_path, 'model_weights.pt')
 
 
 class MLP(nn.Module):
@@ -65,3 +94,63 @@ class ClipCaptionPrefix(ClipCaptionModel):
         super(ClipCaptionPrefix, self).train(mode)  # train the model
         self.gpt.eval()  # turn on the evaluation mode on the model
         return self
+
+
+def generate_beam(model, tokenizer, beam_size: int = 5, prompt=None, embed=None, entry_length=67,
+                  temperature=1., stop_token: str = '.'):
+    model.eval()
+    stop_token_index = tokenizer.encode(stop_token[0])
+    tokens = None
+    scores = None
+    device = next(model.parameters()).device  # we do not have used the device
+    seq_lengths = torch.ones(beam_size, device=device)
+    is_stopped = torch.zeros(beam_size, device=device, dtype=torch.bool)
+    with torch.no_grad():
+        if embed is not None:
+            generated = embed
+        else:
+            if tokens is None:
+                tokens = torch.tensor(tokenizer.encode(prompt))
+                tokens = tokens.unsqueeze(0).to(device)
+                generated = model.gpt.transformer.wte(tokens)
+        for i in range(entry_length):
+            outputs = model.gpt(inputs_embeds=generated)
+            logits = outputs.logits
+            logits = logits[:, -1, :] / (temperature if temperature > 0 else 1.0)
+            logits = logits.softmax(-1).log()
+            if scores is None:
+                scores, next_tokens = logits.topk(beam_size)
+                generated = generated.expand(beam_size, *generated.shape[1:])
+                next_tokens, scores = next_tokens.permute(1, 0), scores.squeeze(0)
+                if tokens is None:
+                    tokens = next_tokens
+                else:
+                    tokens = tokens.expand(beam_size, *tokens.shape[1:])
+                    tokens = torch.cat((tokens, next_tokens), dim = 1)
+            else:
+                logits[is_stopped] = -float(np.inf)
+                logits[is_stopped, 0] = 0
+                scores_sum = scores[:, None] + logits
+                seq_lengths[~is_stopped]+=1
+                scores_sum_average = scores_sum / seq_lengths[:, None]
+                scores_sum_average, next_tokens = scores_sum_average.view(-1).topk(beam_size, -1)
+                next_tokens_source = next_tokens // scores_sum.shape[1]
+                seq_lengths = seq_lengths[next_tokens_source]
+                next_tokens = next_tokens % scores_sum.shape[1]
+                next_tokens = next_tokens.unsqueeze(1)
+                tokens = tokens[next_tokens_source]
+                tokens = torch.cat((tokens, next_tokens), dim = 1)
+                generated = generated[next_tokens_source]
+                scores = scores_sum_average * seq_lengths
+                is_stopped = is_stopped[next_tokens_source]
+            next_token_embed = model.gpt.transformer.wte(next_tokens.squeeze()).view(generated.shape[0], 1, -1)
+            generated = torch.cat((generated, next_token_embed), dim = 1)
+            is_stopped = is_stopped + next_tokens.eq(stop_token_index).squeeze()
+            if is_stopped.all():
+                break
+    scores = scores / seq_lengths
+    output_list = tokens.cpu().numpy()
+    output_texts = [tokenizer.decode(output[:int(length)]) for output, length in zip(output_list, seq_lengths)]
+    order = scores.argsort(descending = True)
+    output_texts = [output_texts[i] for i in order]
+    return output_texts
